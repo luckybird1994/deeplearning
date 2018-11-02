1d82aa554b08b2e2d2a7f1a3a6ee0d1a94a29e3f
luckybird1994 <513789800@qq.com>
2018-11-02 09:54:53 +0800

    change Create_Voc_GT.py and tell how to create voc ground truth

--- Create_Voc_GT.py
+++ Create_Voc_GT.py
@@ -1,11 +1,5 @@
-import os
-import sys
 import cv2
 import numpy as np
-
-SAVE_VOC_GT_IMAGE_PATH = 'datasets/VOCGT/image'
-BASE_GT_PATH = 'datasets/VOCdevkit/VOC2012/SegmentationClass'
-BASE_VOC_GT_TXT_PATH = 'datasets/VOCGT/txt'
 
 LABEL_ARRAYS = np.array( [ [ [128,0,0],[0,128,0],[128,128,0],[0,0,128],[128,0,128], [0,128,128],[128,128,128],
                              [64,0,0],[192,0,0],[64,128,0],[192,128,0],[64,0,128],[192,0,128],[64,128,128],[192,128,128],
@@ -14,54 +8,25 @@ LABEL_ARRAYS = np.array( [ [ [128,0,0],[0,128,0],[128,128,0],[0,0,128],[128,0,12
 LABELS = ['aeroplane','bicycle','bird','boat','bottle','bus','car','cat','chair','cow','diningtable',
           'dog','horse','motorbike','person','pottedplant','sheep','sofa','train','tvmonitor']
 
-def ground_truth( new_filenames, len_newfiles,k,label,type):
+label = 0
 
-    z = 0
-    folder = os.path.join(SAVE_VOC_GT_IMAGE_PATH, type, LABELS[label])
-    os.makedirs(folder)
-    while z < len_newfiles:
+for label in range( 20 ):
 
-        new_gt_array = np.zeros( (128, 258, 3), dtype=np.uint8 )
-        filename = new_filenames[z][0:11]
+    classes = LABELS[label]
+    num = 0
+    tot_class_num = 100 # the tot num of this class
 
-        gt_image_path = os.path.join(BASE_GT_PATH, filename + '.png')
-        print('\n>>gt_image_path', gt_image_path)
+    for num in range( tot_class_num ):
+        img = cv2.imread("the path you save class **** gt images")  # if label = 0, and you will process aeroplane images
+        w,h,c = img.shape
 
-        img = cv2.imread( gt_image_path, cv2.IMREAD_COLOR )
-        img = cv2.resize(img, (256, 128), interpolation=cv2.INTER_AREA)
-        for i in range(128):
-            for j in range(256):
-                if img[i, j, 2] == LABEL_ARRAYS[0,label,0] and img[i, j, 1] == LABEL_ARRAYS[0,label,1] and img[i, j, 0] == LABEL_ARRAYS[0,label,2]:
-                    new_gt_array[i, j, 0] = 255
-                    new_gt_array[i, j, 1] = 255
-                    new_gt_array[i, j, 2] = 255
+        #attention: cv2.imread is bgr image
+        for i in range( w ):
+            for j in range( h ):
+                if img[i, j, 2] == LABEL_ARRAYS[0, label, 0] and img[i, j, 1] == LABEL_ARRAYS[0, label, 1] and img[i, j, 0] == LABEL_ARRAYS[0, label, 2]:
+                    img[i, j, 0] = 255
+                    img[i, j, 1] = 255
+                    img[i, j, 2] = 255
+        cv2.imwrite( " the path you want to save your file", img )
 
-        save_name = os.path.join( SAVE_VOC_GT_IMAGE_PATH, type,LABELS[label],filename + '.png')
-        cv2.imwrite( save_name,new_gt_array )
-        k += 1
-        z += 1
-
-    return k
-
-def create_ground_truth( type = 'train' ):
-
-    label = 0
-    k = 0
-
-    while label <= 19:
-
-        split = LABELS[label]
-
-        split_file_path = os.path.join( BASE_VOC_GT_TXT_PATH,type,split +'.txt' )
-        print( ">> split_file_path", split_file_path )
-
-        with open(split_file_path) as f:
-            filenames = f.readlines()
-
-
-        k = ground_truth( new_filenames = filenames, len_newfiles = len(filenames) ,k = k,label=label,type = type )
-        print( "%dFinshed" % label )
-        label += 1
-    print(">> k", k)
-    return
 
